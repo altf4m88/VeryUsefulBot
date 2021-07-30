3f9b862ca477efe78a58e7077a06e3f399e0e6f6
altf4m88 <fadhilmauladhani@gmail.com>
2021-07-30 15:54:57 +0700

    and thus, this useful bot was born

---
+++ commands/introduce.js
@@ -0,0 +1,18 @@
+const Discord = require("../node_modules/discord.js");
+
+module.exports = {
+    name: 'introduce',
+    description: 'let the bot introduce itself',
+    usage:'|introduce',
+    example: '|introduce',
+    execute(message){
+        if(message.author.id !== "439976892343517184") return message.channel.send("Nope, you're not my master");
+        const embed = new Discord.MessageEmbed()
+        .setTitle('Let Me Introduce Myself')
+        .addField(`I am PencatatSolat`, `Jika itu menyebalkan pasti ada jalan`, false)
+        .setFooter(`aquila non captat muscas`)
+        .setColor('RANDOM')
+
+        message.channel.send(embed)
+    }
+}
---
+++ commands/ping.js
@@ -0,0 +1,10 @@
+module.exports = {
+    name: 'ping',
+    description: 'ping command',
+    usage:'&ping',
+    example: '$ping',
+    execute(message){
+        const timeTaken = Date.now() - message.createdTimestamp;
+        message.reply(`This message had a latency of ${timeTaken}ms.`);
+    }
+}
