2d94f141f927be84205f15883db9e935f57deca8
fadhilmauladhani <62723837+altf4m88@users.noreply.github.com>
2021-08-02 13:49:21 +0000

    update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,17 +8,18 @@ module.exports = {
 
         if(args[0] == undefined){
             embed.setColor('RANDOM')
-	        .setTitle(':book: PencatatSolat')
+	        .setTitle('☪️ PencatatSolat')
 	        .setAuthor('PencatatSolat', 'https://media-exp1.licdn.com/dms/image/C510BAQG1Nyx-6PqmhQ/company-logo_200_200/0/1558518784151?e=1635984000&v=beta&t=nwSVnkoBGnTTtYY3w_JTy88RZ8esHf_fTZW8zha8e-8')
 	        .setDescription('prefixnya `&`, ketik &help <command-name> untuk informasi lebih lanjut setiap command')
 	        .setThumbnail('https://media-exp1.licdn.com/dms/image/C510BAQG1Nyx-6PqmhQ/company-logo_200_200/0/1558518784151?e=1635984000&v=beta&t=nwSVnkoBGnTTtYY3w_JTy88RZ8esHf_fTZW8zha8e-8')
-            .addField('AUTO ABSEN', '`absenin-wajib`, `absenin-dhuha`, `absenin-tahajud`', false)
-            .addField('LINK ABSEN', '`link-solat`, `link-solat-auto`', false)
-            .addField('MISC', '`help`, `introduce`', false)
-            .addField('UTILITY', '`absensi-notifier`, `ping`, `bug-report`', false)
-            .addField('GitHub', 'Plz star my dev repo https://github.com/altf4m88/VeryUsefulBot', false)
+            .addField('🕌  AUTO ABSEN', '`absenin-wajib`, `absenin-dhuha`, `absenin-tahajud`', false)
+            .addField('🕋  LINK ABSEN', '`link-solat`, `link-solat-auto`', false)
+            .addField('⌨️  MISC', '`help`, `introduce`', false)
+            .addField('🧰  UTILITY', '`absensi-notifier`, `ping`, `bug-report`', false)
+            .addField('⭐  GitHub', 'Plz star my dev repo https://github.com/altf4m88/VeryUsefulBot', false)
+            .addField('Contributor', 'altf4m88 | Metclocker | Saekyo', false)
             .setTimestamp()
-            .setFooter('Semoga Berkah');
+            .setFooter('Semoga Berkah 🤠');
 
             return message.channel.send(embed);
         }else if(args.length > 0){
@@ -26,12 +27,12 @@ module.exports = {
             try {
                 const command = require(`./${commandName}.js`);
                 embed.setColor('RANDOM')
-                .setTitle(':book: PencatatSolat &help')
+                .setTitle('🕋 PencatatSolat &help')
                 .setAuthor('PencatatSolat', 'https://media-exp1.licdn.com/dms/image/C510BAQG1Nyx-6PqmhQ/company-logo_200_200/0/1558518784151?e=1635984000&v=beta&t=nwSVnkoBGnTTtYY3w_JTy88RZ8esHf_fTZW8zha8e-8')
                 .setDescription(`${command.description}`)
                 .addField('Usage', `\`${command.usage}\``, true)
                 .addField('Example', `\`${command.example}\``, true)
-                .setFooter('Semoga Berkah');
+                .setFooter('Semoga Berkah 🤠');
 
                 return message.channel.send(embed);
             } catch(err){
