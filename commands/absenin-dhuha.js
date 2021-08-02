c813f7687ea648d7de4a0ab9fdb29c115e01fe64
Suryo Mujahid <suryomujahid@gmail.com>
2021-08-02 15:55:51 +0700

    fix bugs, add hadist message

--- commands/absenin-dhuha.js
+++ commands/absenin-dhuha.js
@@ -1,5 +1,9 @@
 const today = new Date;
 const fetch = require("node-fetch");
+const rawi = {
+    id: ["abu-daud", "ahmad", "bukhari", "darimi", "ibnu-majah", "malik", "muslim", "nasai", "tirmidzi"],
+    available: [3000, 20, 2000, 1000, 2000, 500, 1500, 4000, 3625]
+}
 let year = today.getFullYear();
 let month = ("0" + (today.getMonth() + 1)).slice(-2);
 let date = ("0" + today.getDate()).slice(-2);
@@ -23,15 +27,36 @@ module.exports = {
             }
         );
 
-        let replyEmbed = new Discord.MessageEmbed()
-        .setTitle('Sukses absen Dhuha')
-        .setColor('RANDOM')
-        .setAuthor('PencatatSolat', 'https://media-exp1.licdn.com/dms/image/C510BAQG1Nyx-6PqmhQ/company-logo_200_200/0/1558518784151?e=1635984000&v=beta&t=nwSVnkoBGnTTtYY3w_JTy88RZ8esHf_fTZW8zha8e-8')
-        .setDescription('https://bit.ly/HasilSDC2122')
-        .setTimestamp()
-        .setFooter(`Diminta oleh ${message.author.username}`);
-        
-        return message.channel.send(replyEmbed);
+        fetch(`https://api.hadith.sutanlab.id/books/${rawi.id[rawiIndex]}/${Math.floor(Math.random() * rawi.available[rawiIndex])}`)
+            .then(response => response.json())
+            .then(async json => {
+                let name = json['data']['name'];
+                let no = json['data']['contents']['number'];
+                let contentId = json['data']['contents']['id'];
+
+                let replyEmbed = new Discord.MessageEmbed()
+                .setTitle(`Sukses absen solat Dhuha`)
+                .setColor('RANDOM')
+                .setAuthor('PencatatSolat', 'https://media-exp1.licdn.com/dms/image/C510BAQG1Nyx-6PqmhQ/company-logo_200_200/0/1558518784151?e=1635984000&v=beta&t=nwSVnkoBGnTTtYY3w_JTy88RZ8esHf_fTZW8zha8e-8')
+                .setDescription(`${contentId}`)
+                .addField(`${name}-${no}`, '\u200b')
+                .addField('https://bit.ly/HasilSDC2122', '\u200b')
+                .setTimestamp()
+                .setFooter(`Diminta oleh ${message.author.username}`);
+
+                return message.channel.send(replyEmbed)
+            })
+            .catch((error) => {
+                let replyEmbed = new Discord.MessageEmbed()
+                        .setTitle(`Sukses absen solat Dhuha`)
+                        .setColor('RANDOM')
+                        .setAuthor('PencatatSolat', 'https://media-exp1.licdn.com/dms/image/C510BAQG1Nyx-6PqmhQ/company-logo_200_200/0/1558518784151?e=1635984000&v=beta&t=nwSVnkoBGnTTtYY3w_JTy88RZ8esHf_fTZW8zha8e-8')
+                        .setDescription('https://bit.ly/HasilSDC2122')
+                        .setTimestamp()
+                        .setFooter(`Diminta oleh ${message.author.username}`);
+
+                return message.channel.send(replyEmbed)
+            });
     }
 
 }
--- commands/absensi-notifier.js
+++ commands/absensi-notifier.js
@@ -1,4 +1,12 @@
 const cron = require('cron').CronJob;
+const fetch = require("node-fetch");
+const rawi = {
+    id: ["abu-daud", "ahmad", "bukhari", "darimi", "ibnu-majah", "malik", "muslim", "nasai", "tirmidzi"],
+    // name: [
+    //     "HR. Abu Daud", "HR. Ahmad", "HR. Bukhari", "HR. Darimi", "HR. Ibnu Majah", "HR. Malik", "HR. Muslim", "HR. Nasai", "HR. Tirmidzi",
+    // ],
+    available: [4419, 4305, 6638, 2949, 4285, 1587, 4930, 5364, 3625]
+}
 
 module.exports = {
     name: 'absensi-notifier',
@@ -6,10 +14,11 @@ module.exports = {
     usage:'&absensi-notifier <enable> <time (optional)>',
     example: '&absensi-notifier enable 20.30',
     execute(Discord, message, args){
-        if(message.guild.id != process.env.GUILD_ID) return message.channel.send("Fitur ini hanya bisa dilakukan di server RPLMepo Fan Club");
+        // if(message.guild.id != process.env.GUILD_ID) return message.channel.send("Fitur ini hanya bisa dilakukan di server RPLMepo Fan Club");
+
         if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send("Fitur ini hanya bisa dilakukan oleh Administrator");
         if(args[0] == undefined || args[0] != 'enable') return message.channel.send("Masukan kondisi nak");
-
+        
         if(args[1] !== undefined){
             if (isNaN(args[1]) || args[1].length > 5 || args[1].length < 5) {
                 let replyEmbed = new Discord.MessageEmbed()
@@ -43,14 +52,8 @@ module.exports = {
             }
 
             let notifierMessage = new cron(`00 ${minute} ${hour} * * *`, () => {
-                let replyEmbed = new Discord.MessageEmbed()
-                .setTitle(`Jangan lupa untuk absensi sholat hari ini!`)
-                .setColor('RANDOM')
-                .setAuthor('PencatatSolat', 'https://media-exp1.licdn.com/dms/image/C510BAQG1Nyx-6PqmhQ/company-logo_200_200/0/1558518784151?e=1635984000&v=beta&t=nwSVnkoBGnTTtYY3w_JTy88RZ8esHf_fTZW8zha8e-8')
-                .setDescription(`<@&${process.env.ROLE_ID}>`)
-                .setTimestamp()
-
-                message.channel.send(replyEmbed);
+                message.channel.send(`<@&${process.env.ROLE_ID}>` + 'Jangan lupa absensi sholat hari ini!');
+                
             }, null, true, 'Asia/Jakarta');
 
             notifierMessage.start();
